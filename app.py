a33252fc3b8a26d180b89f14bbc1289758e4d8f6
yavuz yildirim <yvzyldrm@gmail.com>
2024-05-22 01:35:27 +0300

    flask code

--- app.py
+++ app.py
@@ -1,6 +1,45 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify
+import requests
+import json
+import threading
+import time
+from flask_cors import CORS  # CORS eklentisi
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+CORS(app)  # CORS eklentisini uygula
+json_file = "doviz.json"
+
+def download_and_save_json(url, file_path):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(file_path, 'wb') as f:
+                f.write(response.content)
+            print(f"JSON dosyası başarıyla indirildi ve kaydedildi: {file_path}")
+        else:
+            print(f"Hata: HTTP {response.status_code} - {response.reason}")
+    except Exception as e:
+        print(f"Hata: {e}")
+
+def update_json_periodically(url, file_path, interval):
+    while True:
+        download_and_save_json(url, file_path)
+        time.sleep(interval)
+
+@app.route('/doviz')
+def get_doviz():
+    try:
+        with open(json_file, 'r') as f:
+            data = json.load(f)
+            return jsonify(data)
+    except FileNotFoundError:
+        return "Hata: JSON dosyası bulunamadı.", 404
+
+if __name__ == "__main__":
+    # İlk indirme ve ardından belirli aralıklarla güncelleme işlemi için bir thread başlatın
+    download_thread = threading.Thread(target=update_json_periodically, args=("https://api.genelpara.com/embed/doviz.json", json_file, 300))
+    download_thread.daemon = True
+    download_thread.start()
+
+    # Flask sunucusunu başlatın
+    app.run(debug=True)
